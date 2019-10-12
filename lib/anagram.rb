47284f9c688f8362d83971e3588c9a8c49bd6337
cedric e goddard <8ceg6@comcast.net>
2019-10-12 16:13:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+attr_accessor :aw 
+
+  def initialize(aw)
+    @aw= aw
+  end 
+  
+  def match(a_word)
+    a_word.split%w()
+  
+  end  
+
+
+
+end  
